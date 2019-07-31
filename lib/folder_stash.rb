b875732e8dcb3be677257116339fbc8ad904c6b0
Martin Stein <loveablelobster@fastmail.fm>
2019-07-31 15:02:03 +0200

    file_usher

---
+++ lib/folder_stash.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+require_relative 'folder_stash/file_usher'
+
+#
+module FolderStash
+end
---
+++ lib/folder_stash/file_usher.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+module FolderStash
+  #
+  class FileUsher
+    # The directory
+    attr_reader :directory
+
+    def initialize(dir, nesting: 2, items: 1000)
+      @directory = dir
+      @nesting = nesting
+      @items_per_dir = items
+    end
+
+    def current_dir
+      # return dir for symlink if symlink exists
+      #
+      # create symlink
+    end
+
+    def store(file); end
+  end
+end
